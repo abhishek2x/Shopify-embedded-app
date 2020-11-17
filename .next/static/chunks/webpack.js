00fbf89fb2bddf9e1c843dbff3663c7459a8a0ed
abhishek2x <abhisheksrivastavabbn@gmail.com>
2020-11-17 11:24:55 +0530

    next move

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e2d078a03764869765f";
+/******/ 	var hotCurrentHash = "50edaed5046e6e5d7f1a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
